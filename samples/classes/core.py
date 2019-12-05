b65e1edda769f8031ec888063b2f0cd012382742
Wilke <Wilkenfeld@users.noreply.github.com>
2019-12-05 17:01:45 +0100

    create core.py

---
+++ samples/classes/core.py
@@ -0,0 +1,12 @@
+# unit to scale lines and nodes 
+# (e.g.: if unit = 3 and line = 3, the final line = line * unit = 9)
+unit = 3
+
+# Dimensions of the root (hor, ver)
+dimensions = (1200, 800)
+
+# Dimension of the canvas (hor, ver)
+canvasDimensions = (800, 800)
+
+# Pure lenght of the side of a node
+nodeDimension = 2
