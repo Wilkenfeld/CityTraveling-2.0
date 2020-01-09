9dfa61f2ae979e510ed0ae2d1d5dfc3958ae77ce
Wilke <Wilkenfeld@users.noreply.github.com>
2020-01-09 14:11:47 +0100

    Update graph.py

--- samples/classes/graph.py
+++ samples/classes/graph.py
@@ -1,5 +1,6 @@
 from .node import Node as node
 import json
+from tkinter import messagebox
 
 class Graph():
     
@@ -9,15 +10,19 @@ class Graph():
     def __init__(self, jsonFile = None):
         if (jsonFile != None and Graph.number_of_graphs == 0):
             Graph.number_of_graphs += 1
+
             with open(jsonFile) as file:
-               nodesRaw = json.load(file)
-               for nodeRaw in nodesRaw:
-                self.nodes.append(
-                    node(
-                        nodeRaw["id"], 
-                        nodeRaw["closeTo"], 
-                        nodeRaw["nodeType"], 
-                        position = nodeRaw["position"]
-                    )
-                )
+                try:
+                    nodesRaw = json.load(file)
+                    for nodeRaw in nodesRaw:
+                        self.nodes.append(
+                            node(
+                                nodeRaw["id"], 
+                                nodeRaw["closeTo"], 
+                                nodeRaw["nodeType"], 
+                                position = nodeRaw["position"]
+                            )
+                        )
+                except:
+                    messagebox.showerror("Error loading the file", "This file is not a city configuration file")
 
