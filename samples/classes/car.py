7f1d1c4dba035421ea0f6f66aa0e5dc5cb94a986
Wilke <Wilkenfeld@users.noreply.github.com>
2020-01-13 18:25:40 +0100

    Update car.py

--- samples/classes/car.py
+++ samples/classes/car.py
@@ -1,9 +1,20 @@
+from roadMaker import roadMaker as RM
+
 class Car():
 
-    def __init__(self, id, carType, startPoint, endPoint, status):
+    self.path = []
+    
+
+    def __init__(self, id, carType, startPoint, endPoint, length, status):
         self.id = id
         self.type = carType
         self.startPoint = startPoint
         self.endPoint = endPoint
         self.status = status
-        self.props = (id, carType, startPoint, endPoint, status)
+        self.props = (id, carType, startPoint, endPoint, length, status)
+
+        self.currentPosition = start
+        self.requestPath()
+
+    def requestPath(self):
+        self.path = RM.makePath(self.startPoint, self.endPoint)
