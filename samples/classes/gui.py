5c396d304727013d9bff8fa9ecbd6137fbf42c8a
Wilke <Wilkenfeld@users.noreply.github.com>
2020-01-23 17:01:42 +0100

    Update gui.py

--- samples/classes/gui.py
+++ samples/classes/gui.py
@@ -1,3 +1,4 @@
+
 import math as Math
 from functools import partial
 from tkinter import *
@@ -23,168 +24,167 @@ from .commandsHandler import CommandHandler as handler
 # Main Window
 root = Tk()
 
-class GUI():
+void = None
 
-    graph = None
+def start():
+
+    global status
     status = "start"
-
-    def main(self):
         
-        # Creation of the main elements of the page
-        cityScheme = None
-        self.cityModel = Canvas(master=root, 
-                                bg="grey", 
-                                width="500", 
-                                height="500")
+    # Creation of the main elements of the page
+    cityScheme = None
+    global cityModel
+    cityModel = Canvas(master=root, 
+                            bg="grey", 
+                            width="500", 
+                            height="500")
 
-        menu = Frame(master=root, borderwidth=3, height="500", width="100", pady="30", padx="30")
-        title = Label(master=menu, text="CitySim! (ALPHA RELEASE 1.0)")
-        JSONMenu = Button(master = menu, text="Open File", padx="5", pady="5", command=self.openJSONFile)
-        runButton = Button(master = menu, text="Run", padx="5", pady="5", command=self.run)
+    menu = Frame(master=root, borderwidth=3, height="500", width="100", pady="30", padx="30")
+    title = Label(master=menu, text="CitySim! (ALPHA RELEASE 1.0)")
+    JSONMenu = Button(master = menu, text="Open File", padx="5", pady="5", command=openJSONFile)
+    runButton = Button(master = menu, text="Run", padx="5", pady="5", command=run)
 
-        self.carsMenu = Frame(master = root, bg ="black", width="500")
-        self.carsList = Frame(master = self.carsMenu, bg = "green", width="500")
-        self.manageBar = Frame(master = self.carsMenu, bg = "blue", width = "500")
+    global carsMenu
+    carsMenu = Frame(master = root, bg ="black", width="500")
+    global carsList
+    carsList = Frame(master = carsMenu, bg = "green", width="500")
+    global manageBar
+    manageBar = Frame(master = carsMenu, bg = "blue", width = "500")
 
-        # Layout of the window
-        root.title("CitySim")
-        title.pack(side="top")
-        JSONMenu.pack(side="top")
-        runButton.pack()
+    # Layout of the window
+    root.title("CitySim")
+    title.pack(side="top")
+    JSONMenu.pack(side="top")
+    runButton.pack()
 
-        # centers the menu
-        root.grid_columnconfigure(1, weight="1")
+    # centers the menu
+    root.grid_columnconfigure(1, weight="1")
 
-        self.cityModel.grid(column="0", row="0")
-        menu.grid(column="1", row="0")
+    cityModel.grid(column="0", row="0")
+    menu.grid(column="1", row="0")
 
-        self.carsMenu.columnconfigure(0, weight=1)
-        self.carsMenu.grid(column="0", row="1", sticky="NSEW")
-        self.carsList.grid(column="0", row="0", sticky="NSEW")
-        self.manageBar.grid(column="0", row="1", sticky="NSEW")
+    carsMenu.columnconfigure(0, weight=1)
+    carsMenu.grid(column="0", row="1", sticky="NSEW")
+    carsList.grid(column="0", row="0", sticky="NSEW")
+    manageBar.grid(column="0", row="1", sticky="NSEW")
 
-        root.grid_propagate(True)
-        root.resizable(True, True)
-        root.geometry("1200x680")
+    root.grid_propagate(True)
+    root.resizable(True, True)
+    root.geometry("1200x680")
+    
+    # Execution
+    root.mainloop()
+
+def createPositionsDictionary():
+
+    # Positions of the nodes on the plot
+    global position_dict
+    position_dict = dict()
+
+    # Adds the values to the dictionary
+    for node in graph_obj.nodes:
+        position_dict[node.id] = node.position
+
+# Gets the JSON file
+def openJSONFile():
+    JSONFilePath = filedialog.askopenfilename(title="Select JSON file", 
+                                                    filetypes=[("JSON Files", ".json")])
+    global graph_obj
+    graph_obj = Graph(JSONFilePath)
+    createPositionsDictionary()
+
+    createCityImage()
+    createCarsMenu()
+
+def createCityImage():
+    # The dimension of the widget is calculated by x * dpi and y * dpi
+    global fig
+    fig = Figure(figsize=(5, 5), dpi=100)
+    global sp
+    sp = fig.add_subplot(111)
+
+    sp.plot([n.position[0] for n in graph_obj.nodes], [n.position[1] for n in graph_obj.nodes], 'ro')
+
+    # Draws the streets
+    for node in graph_obj.nodes:
         
-        # Execution
-        root.mainloop()
+        targets = [position_dict[node.closeTo[i][0]] for i, _ in enumerate(node.closeTo)]
 
-    # Gets the JSON file
-    def openJSONFile(self):
-        JSONFilePath = filedialog.askopenfilename(title="Select JSON file", 
-                                                        filetypes=[("JSON Files", ".json")])
-        self.graph_obj = Graph(JSONFilePath)
-
-        self.createCityImage()
-        self.createCarsMenu()
-
-
-    # Creates the networkx graph
-    def nxGraph(self, graph_obj):
-        # Creates the graph
-        graph = nx.DiGraph()
-
-        # Positions of the nodes on the plot
-        self.position_dict = dict()
-
-        # Adds the nodes to the graph
-        for node in graph_obj.nodes:
-            graph.add_node(node.id)
-            # Adds the edges
-            for link in node.closeTo:
-                graph.add_edge(node.id, link[0], weight=link[1])
+        for t in targets:
+            l = lines.Line2D([node.position[0], t[0]], [node.position[1], t[1]])
+            sp.add_line(l)  
             
-            # Adds the values to the dictionary
-            self.position_dict[node.id] = node.position
+    # Draws the nodes
+    sp.plot([n.position[0] for n in graph_obj.nodes], [n.position[1] for n in graph_obj.nodes], 'ro')
 
-        return graph
-
-    # shows the city
-    def createCityImage(self):
-
-        nx_graph = self.nxGraph(self.graph_obj)
-
-        # The dimension of the widget is calculated by x * dpi and y * dpi
-        self.fig = Figure(figsize=(5, 5), dpi=100)
-        self.sp = self.fig.add_subplot(111)
-
-        self.sp.plot([n.position[0] for n in self.graph_obj.nodes], [n.position[1] for n in self.graph_obj.nodes], 'ro')
-
-        # Draws the streets
-        for node in self.graph_obj.nodes:
-            
-            targets = [self.position_dict[node.closeTo[i][0]] for i, _ in enumerate(node.closeTo)]
-
-            for t in targets:
-                l = lines.Line2D([node.position[0], t[0]], [node.position[1], t[1]])
-                self.sp.add_line(l)  
-                
-        # Draws the nodes
-        self.sp.plot([n.position[0] for n in self.graph_obj.nodes], [n.position[1] for n in self.graph_obj.nodes], 'ro')
-
-        # Binds the city to a tkinter widget and draws it
-        self.canvas = FigureCanvasTkAgg(self.fig, master=self.cityModel)
-        self.canvas.get_tk_widget().grid(column=0, row=0)   
-        
-        self.canvas.draw()
+    # Binds the city to a tkinter widget and draws it
+    global canvas 
+    canvas = FigureCanvasTkAgg(fig, master=cityModel)
+    canvas.get_tk_widget().grid(column=0, row=0)   
+    
+    canvas.draw()
 
 
-    def run(self):
-        self.status = "running"
-        self.anim()
+def run():
+    global status
+    status = "running"
+    anim()
 
-    def createCarsMenu(self):
-        # creates the header of the table
-        headers = ["Car Number", "Type", "Start", "Destination", "Length", "Status"]
-        for i, head in enumerate(headers):
+def createCarsMenu():
+    # creates the header of the table
+    headers = ["Car Number", "Type", "Start", "Destination", "Length", "Status"]
+    for i, head in enumerate(headers):
 
-            tmp = Label(master=self.carsList, text=head, bg = "white", borderwidth=1, relief="groove")
-            self.carsList.columnconfigure(i, weight=1)
-            tmp.grid(column = i, row = 0, sticky="NSEW")
+        tmp = Label(master=carsList, text=head, bg = "white", borderwidth=1, relief="groove")
+        carsList.columnconfigure(i, weight=1)
+        tmp.grid(column = i, row = 0, sticky="NSEW")
 
-        self.void = Label(master = self.carsList, text="Click Add to add a new car!")
-        self.void.grid(row = 0, column = 0, columnspan=len(headers))
+    void = Label(master = carsList, text="Click Add to add a new car!")
+    void.grid(row = 0, column = 0, columnspan=len(headers))
 
-        # creates the "user" bar
-        self.buttons = []
-        headers = ["Play", "Pause", "Add", "Cancel", "Save", "Load"]
+    # creates the "user" bar
+    global buttons
+    buttons = []
+    headers = ["Play", "Pause", "Add", "Cancel", "Save", "Load"]
 
-        for i, head in enumerate(headers):
-            self.buttons.append({
-                "command": head,
-                "button": Button(master = self.manageBar, text = head, command = partial(handler.handleCommand, head, self))
-            })
-            self.manageBar.columnconfigure(i, weight=1)
-            self.buttons[i]["button"].grid(row=0, column = i, sticky="NSEW")
+    for i, head in enumerate(headers):
+        buttons.append({
+            "command": head,
+            "button": Button(master = manageBar, text = head, command = partial(handler.handleCommand, head, ))
+        })
+        manageBar.columnconfigure(i, weight=1)
+        buttons[i]["button"].grid(row=0, column = i, sticky="NSEW")
 
-    def anim(self):
-        # if the simulation is running
-        # runs the animation
-        isRunning = self.status == "running"
-        self.animID = animation.FuncAnimation(self.fig, self.draw, interval=10)
-        print (self.status)
-        if isRunning:
-            self.animID.event_source.start()
-            print("animation started")
-        else: 
-            self.animID.event_source.stop()
-            print("animation stopped")
-            
-    def draw(self, frame):
-        print("draw")
-        print(frame)
-        self.sp.add_patch(rect((1, 1), 2, 3))
-        self.canvas.show()
+def anim():
+    # if the simulation is running
+    # runs the animation\
+    isRunning = status == "running"
+    global animID
+    animID = animation.FuncAnimation(fig, draw, frames=200, interval=100)
 
-    # inserts cars in the table (called when a file is loaded or when the Add button is pressed)
-    def addCar(self):
-        self.void.destroy()
+    print ("status: " + status)
+    print("fig: ", fig)
+    print("animID:", animID)
+    print("draw:", draw)
+    if isRunning:
+        print("animation started")
+    else: 
+        print("animation stopped")
 
-        for i, car in enumerate(cars):
-            for j, prop in enumerate(car.props):
-                tmp = Label(master = self.carsList, text=prop, borderwidth="1", relief = "sunken", pady="5")
-                tmp.columnconfigure(j, weight = 1)
-                tmp.grid(row = i + 1, column = j, sticky="NSEW")
-                # self.sp.add_patch(rect((2,2), car.length, car.length))
-                self.canvas.draw()
+def draw(frame):
+    print("draw")
+    print(frame)
+    sp.add_patch(rect((2, 2), 1, 1))
+
+# inserts cars in the table (called when a file is loaded or when the Add button is pressed)
+def addCar():
+    void.destroy()
+
+    for i, car in enumerate(cars):
+        for j, prop in enumerate(car.props):
+            tmp = Label(master = carsList, text=prop, borderwidth="1", relief = "sunken", pady="5")
+            tmp.columnconfigure(j, weight = 1)
+            tmp.grid(row = i + 1, column = j, sticky="NSEW")
+            # sp.add_patch(rect((2,2), car.length, car.length))
+            canvas.draw()
+
