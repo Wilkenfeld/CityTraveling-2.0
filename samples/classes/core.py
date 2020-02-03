3f351f7b769a01e2e597d7cbd60a30caec546356
Wilke <Wilkenfeld@users.noreply.github.com>
2020-02-03 15:18:47 +0100

    Update core.py

--- samples/classes/core.py
+++ samples/classes/core.py
@@ -1,18 +1,51 @@
-# unit to scale lines and nodes 
-# (e.g.: if unit = 3 and line = 3, the final line = line * unit = 9)
-unit = 3
+# This file contains all of the global variables needed, such as the list of cars
 
-# Dimensions of the root (hor, ver)
-dimensions = (1200, 800)
 
-# Dimension of the canvas (hor, ver)
-canvasDimensions = (800, 800)
+def initialize():
 
-# Pure lenght of the side of a node
-nodeDimension = 2
+    # Widgets
+    global cityModel
+    global carsMenu
+    global carsList
+    global manageBar
+    global void
 
-# Cars
-cars = []
+    # handler for the animation
+    global animID
 
-# Status of the app (at the beginning is setted on "start")
-status = "start"
+    # Buttons of the cars list
+    global buttons
+
+    # The plot itself
+    global sp
+
+    # MPL widget containing the plot and the canvas to handle it with tkinter
+    global fig
+    global canvas
+
+    # Graph object
+    global graph_obj
+
+    # Positions of the nodes on the plot
+    global position_dict
+
+    # unit to scale lines and nodes 
+    # (e.g.: if unit = 3 and line = 3, the final line equals line * unit = 9)
+    global unit
+    unit = 3
+
+    # Dimensions of the root (hor, ver)
+    global dimensions
+    dimensions = (1200, 800)
+
+    # Dimension of the canvas (hor, ver)
+    global canvasDimensions
+    canvasDimensions = (800, 800)
+
+    # List of cars on the simulation
+    global cars_common
+    cars_common = []
+
+    # Status of the app (at the beginning is setted on "start")
+    global status
+    status = "start"
