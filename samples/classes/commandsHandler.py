9439e0d758445440fc89a6473a039fc7274b1d17
Wilke <Wilkenfeld@users.noreply.github.com>
2020-02-03 15:18:51 +0100

    Update commandsHandler.py

--- samples/classes/commandsHandler.py
+++ samples/classes/commandsHandler.py
@@ -1,45 +1,38 @@
-from .core import *
+import samples.classes.core as core
 import json
 import tkinter as tk
 import matplotlib.pyplot as plt
 from .car import Car
-import samples.classes.gui as gui
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
                                                NavigationToolbar2Tk)
 from matplotlib.figure import Figure
 
 class CommandHandler():
-    gui = None
     # Main handler
     @staticmethod
     def handleCommand(command):
-        CommandHandler.gui = gui 
         if (command == "Add"): CommandHandler.addCar()
         elif command == "Load": CommandHandler.loadFromFile()
 
     # Add button (adds a car)
     @staticmethod
-    def addCar():
-        print([car for car in cars])
+    def addCar(car):
+        core.cars_common.append(car)
+        print(car)
 
     @staticmethod
     def loadFromFile():
         JSONFilePath = tk.filedialog.askopenfilename(title="Select JSON file", 
                                                         filetypes=[("JSON Files", ".json")])
-        
         # loads the cars from the JSON file
         with open(JSONFilePath) as file:
-            
             try:
                 listOfCars = json.load(file)
                     
                 for car in listOfCars:
-                    # print(car)
-                    cars.append(
-                        Car(car["id"], car["type"], car["startPoint"], car["endPoint"], car["length"], car["status"])
-                    )
-                
-                CommandHandler.gui.addCar()
+                    car_obj = Car(car["id"], car["type"], car["startPoint"], car["endPoint"], car["length"], car["status"])
+                    CommandHandler.addCar(car_obj)
+
             except Exception as e:
-                print("exception >> {}".format(e))
+                print(e)
                 tk.messagebox.showerror("Error loading the file", "The current file isn't a car configuration file")
