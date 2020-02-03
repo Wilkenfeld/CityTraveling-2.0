fae98c6807ac4e00296b4aa0f2f8d894454e93e6
Wilke <Wilkenfeld@users.noreply.github.com>
2020-02-03 18:19:32 +0100

    Update car.py

--- samples/classes/car.py
+++ samples/classes/car.py
@@ -1,19 +1,22 @@
+import samples.classes.core
 class Car():    
 
-    def __init__(self, ID, carType, startPoint, endPoint, length, status):
+    def __init__(self, ID, carType, startPoint, endPoint, length, status, pollution):
         self.id = ID
         self.type = carType
         self.startPoint = startPoint
         self.endPoint = endPoint
         self.length = length
         self.status = status
+        self.pollution = pollution
         self.props = (ID, carType, startPoint, endPoint, length, status)
         self.path = []
 
         self.currentPosition = startPoint
 
     def requestPath(self):
-        pass
+        self.path = core.graph_obj.makePath(self, self.startPoint, self.endPoint)
+        return self.path
 
     def getCurrentPosition(self):
-        pass
+        return self.currentPosition
