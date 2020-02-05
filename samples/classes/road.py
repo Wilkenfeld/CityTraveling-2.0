3adaadad639f4be15a557a7b46b0395c6bd6ab1a
Azale29 <55351005+Azale29@users.noreply.github.com>
2020-02-05 18:31:31 +0100

    tmp

--- samples/classes/road.py
+++ samples/classes/road.py
@@ -2,14 +2,14 @@ import math
 
 class Road():
 
-    def __init__(self, start, end, maxPollution):
+    def __init__(self, start, end, maxPollution = None):
         self.start = start
         self.end = end
         self.maxPollution = maxPollution
 
         self.actualPollution = 0
-        self.actualSpaceLeft = math.sqrt(start**2 + end**2)
-    
+        self.actualSpaceLeft = math.sqrt(int(start)**2 + int(end)**2)
+
     def addCar(self, car):
         isAcceptable = self.actualSpaceLeft >= car.length and self.actualPollution + car.pollution < self.maxPollution
         if isAcceptable:
